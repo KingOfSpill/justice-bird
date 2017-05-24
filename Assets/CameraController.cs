821ed2633d0a163fe0f85761e7a343d98e2552a0
gman1012 <ggoodfl@gmail.com>
2017-05-23 21:08:59 -0400

    Revert "Clamp vertical rotation"
    This reverts commit 2b99e8e0dea8db75bdea51ae7b7d65d78d8a05db.

--- Assets/CameraController.cs
+++ Assets/CameraController.cs
@@ -13,7 +13,7 @@ public class CameraController : MonoBehaviour {
 	void Update () {
 
         // Doing this to keep the  camera level to the ground
-        // Camera.main.transform.rotation = Quaternion.Euler(Camera.main.transform.rotation.eulerAngles.x, Camera.main.transform.rotation.eulerAngles.y, 0);
+        Camera.main.transform.rotation = Quaternion.Euler(Camera.main.transform.rotation.eulerAngles.x, Camera.main.transform.rotation.eulerAngles.y, 0);
 
     }
 }
