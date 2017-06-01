91488c2b1da54cb9f4db9ef4448a3c693fa015a5
Grant Goodman <ggoodfl@gmail.com>
2017-06-01 12:57:19 -0400

    Moved Camera Onto Rails
    Switched the controls to work with a camera on rails.

--- Assets/CameraController.cs
+++ Assets/CameraController.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour {
 
+	public float movSpeed = 2;
+
 	// Use this for initialization
 	void Start () {
 		
@@ -14,6 +16,10 @@ public class CameraController : MonoBehaviour {
 
         // Doing this to keep the  camera level to the ground
         // Camera.main.transform.rotation = Quaternion.Euler(Camera.main.transform.rotation.eulerAngles.x, Camera.main.transform.rotation.eulerAngles.y, 0);
+
+        Vector3 forwardFlat = new Vector3( transform.forward.x, 0, transform.forward.z );
+
+        transform.position += forwardFlat * movSpeed;
 
     }
 }
