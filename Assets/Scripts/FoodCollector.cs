d0876db485360e69e29d3b1443e47236813fb616
sustx <wtyback@knights.ucf.edu>
2017-06-08 16:38:54 -0400

    Outsourced Food Collection
    Made this into its own separate script

---
+++ Assets/Scripts/FoodCollector.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FoodCollector : MonoBehaviour {
+
+	// Food trigger
+	void OnTriggerEnter(Collider other){
+
+		AudioSource pickUp = GetComponent<AudioSource>();
+
+		if (other.gameObject.CompareTag ("Food")) {
+
+			other.gameObject.SetActive (false);
+			pickUp.Play ();
+
+		}
+
+	}
+}
--- Assets/Scripts/Rotate.cs
+++ Assets/Scripts/Rotate.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Rotate : MonoBehaviour {
 
-	public int rotationSpeed = 1; 
+	public float rotationSpeed = 1.5f; 
 	int x = 30, y = -30, z = 30;
 
 	void Update () {
