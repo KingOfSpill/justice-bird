17f762f53c12c3bde991b60045eac62f4eac4313
marcburrell <marcburrell@bellsouth.net>
2017-06-19 21:48:47 -0400

    Scoring UI added
    Added Score that slowly rises to indicate a distace score and increases with poop hits (simulated with a test button)
    Completing Task 28
    Progress on Task 7

---
+++ Assets/Scripts/UIscript.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIscript : MonoBehaviour {
+
+    public Text scoreText;
+    public Button testpoopbutton;
+
+    private int score = 0;
+    private int delay = 0;
+    
+	// Use this for initialization
+	void Start () {
+        //Button poohit = testpoopbutton.GetComponent<Button>();
+        testpoopbutton.onClick.AddListener(poopHit);
+        printScore();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(delay % 100 == 0)
+        {
+            score++;
+        }
+ 
+        printScore();
+        delay++;
+
+	}
+
+void printScore()
+    {
+        scoreText.text = "Score: " + score.ToString();
+    }
+
+    void poopHit()
+    {
+        score += 100;
+    }
+}
